2098c4efc546d307cfab72839c7b46b7dc947f48
meny2333 <meny233@qq.com>
2026-05-23 09:18:46 +0800

    chore: remove #Template to prepare for upstream sync

--- #Template/[Scripts]/Settings/OldCameraSettings.gd
+++
@@ -1,9 +0,0 @@
-class_name OldCameraSettings
-extends Resource
-
-@export var offset: Vector3 = Vector3.ZERO
-@export var rotation: Vector3 = Vector3.ZERO
-@export var scale: Vector3 = Vector3.ONE
-@export var fov: float = 60.0
-@export var follow: bool = true
-@export var distance: float = 0.0
